60971716cb83a321f850c7eb73bb6bd6636d32d3
Thomas Derleth <thomas.derleth@moovel.com>
2021-01-12 17:20:12 +0100

    feat: day 4 (class vs instance)

---
+++ days/4-class-vs-instance/index.test.js
@@ -0,0 +1,28 @@
+const Person = require('./index');
+
+const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+describe('Day 2: Operators', () => {
+  beforeEach(() => {
+    consoleSpy.mockClear();
+  });
+
+  test('1', () => {
+    const input = [-1, 10, 16, 18];
+    const expected = [
+      ['Age is not valid, setting age to 0.'],
+      ['You are young.'],
+      ['You are young.'],
+      ['You are a teenager.'],
+      ['You are old.'],
+    ];
+
+    input.forEach((element) => {
+      const person = new Person(element);
+      person.amIOld();
+    });
+
+    expect(console.log).toBeCalledTimes(5);
+    expect(console.log.mock.calls).toEqual(expected);
+  });
+});
