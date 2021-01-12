761a3a1483b54b446408088b8abd359df8837610
Thomas Derleth <thomas.derleth@moovel.com>
2021-01-12 12:12:27 +0100

    chore: initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: { es6: true },
+  extends: ['plugin:prettier/recommended'],
+  parserOptions: { ecmaVersion: 2017 },
+  rules: {},
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+# 30 Days of Code 🥳
+
+Solutions for 30 Days of Code by HackerRank in JavaScript.
